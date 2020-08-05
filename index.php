9396c0c4a4d676175f73f5106f3e8503acbae88d
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-08-05 18:34:20 -0300

    variable declaration

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+$drinks = [
+  "Latte" => 3.99,
+  "Coffee" => 2.00,
+  "Tea" => 2.50,
+  "Mocha" => 4.50
+];
+
+$pastries = [
+  "Croissant",
+  "Muffin",
+  "Slice of Pie",
+  "Slice of Cake",
+  "Cupcake",
+  "Brownie"
+];
+?>
+
+<h1>Welcome to the Repetitive Cafe</h1>
+
+<h3>Drinks!</h3>
+<ul>
+
+</ul>
+<h3>Pastries! ($2 each)</h3>
+<ul>
+
+</ul>
